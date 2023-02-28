1f94838dc883f35dd08ca3063fee01ad45b0ba63
glcarranzani <glcarranzani@gmail.com>
2023-02-27 21:57:29 -0300

    sesion 14, formik y yup

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
 import Optionalrender from './components/pure/opcionalRender';
+import LoginFormik from './components/pure/forms/loginFormik';
 
 function App() {
   return (
     <div className="App">
       {/* componentes de listado de tareas */}
-      <TaskListComponent />
+      {/* <TaskListComponent /> */}
       {/*  <Ejemplo1></Ejemplo1> */}
       {/* <Ejemplo2></Ejemplo2> */}
       {/* <MiComponenteConContexto></MiComponenteConContexto> */}
@@ -23,6 +24,8 @@ function App() {
       {/* <Father></Father> */}
       {/* ejemplos de renderizado condicional */}
       {/* <Optionalrender /> */}
+      {/* ejemplo formik yup */}
+      <LoginFormik></LoginFormik>
     </div>
   );
 }

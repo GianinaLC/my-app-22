2a9dc10c5c489e916cee7271b298f7a48fe7beb9
glcarranzani <glcarranzani@gmail.com>
2023-03-09 16:07:04 -0300

    gestion de rutas

--- src/AppRoutingOne.js
+++ src/AppRoutingOne.js
@@ -1,7 +1,7 @@
 //alojamos nuestras rutas
 //switch es un conjunto de rutas
 //switch es reemplazado por Routes en v6
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 import Homepage from './pages/home/HomePage';
 import Notfoundpage from './pages/404/NotFoundPage';
@@ -9,8 +9,25 @@ import AboutPage from './pages/about-faqs/AboutPage';
 import ProfilePage from './pages/profile/ProfilePage';
 import Taskpage from './tasks/TasksPage';
 import TaskDetailPage from './tasks/TasksDetailPage';
+import Loginpage from './pages/auth/LoginPage';
+import { useEffect } from 'react';
+import StatePage from './pages/home/StatePage';
 
 function AppRoutingOne() {
+  let logged = false;
+
+  let taskList = [
+    { id: 1, name: 'task1', description: 'primer tarea' },
+    { id: 2, name: 'task2', description: 'segunda tarea' },
+  ]
+
+
+  useEffect(() => {
+    logged = localStorage.getItem('credentials');
+    console.log('user logged', logged)
+  }, [])
+
+
   return (
     <div className="App">
       {/* BrowserRouter llamado como Router */}
@@ -19,19 +36,45 @@ function AppRoutingOne() {
           <aside>
             <Link to='/'> HOME </Link>
             <Link to='/about'> ABOUT </Link>
+            <Link to='/task/1'> task1 </Link>
+            <Link to='/task/2'> task2 </Link>
             {/* <Link to='/faqs'> FAQS </Link> */}
+            <Link to='/login'>Login</Link>
             <Link to='/x404'> 404</Link>
           </aside>
 
           <main>
             <Routes>
               <Route exact path='/' element={< Homepage />} />
+              <Route path='/online-state' element={<StatePage />} />
+              <Route exact path='/login' element={
+                logged ? (
+                  <Navigate replace to="/" />
+                  //**v5 Redirect ,v6 Navigate
+                ) : (
+                  <Loginpage />
+                )} />
               {/* TODO: NO ME ANDA EL PATH COMBINADO */}
               {/* <Route path='/(about|faqs)' element={< AboutPage />} /> */}
               <Route path='/about' element={< AboutPage />} />
-              <Route path='/profile' element={<ProfilePage />} />
+              <Route path='/profile' element={
+                logged ? (
+                  <ProfilePage />
+                ) : (
+                  <Navigate replace to="/login" />
+                )
+              } />
               <Route path='/tasks' element={<Taskpage />} />
-              <Route path='/task/:id' element={<TaskDetailPage />} />
+              {/* <Route
+                exact
+                path='/task/:id'
+                render={
+                  ({ match }) => (<TaskDetailPage task={taskList[match.params.id - 1]} />)
+                }
+              >
+
+              </Route> */}
+              <Route path='/task/:id' element={<TaskDetailPage task={taskList} />} />
               {/* no se usa mas switch ni component */}
               {/* <Route exact path='/' component={Homepage} /> */}
               {/* dejar siempre al final el 404, cuando no encuentra la ruta sigue con la sgte y si no encuentra ninguna sera un 404 */}
--- src/tasks/TasksDetailPage.jsx
+++ src/tasks/TasksDetailPage.jsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom"
 
-const TaskDetailPage = () => {
+const TaskDetailPage = ({task}) => {
     const { id } = useParams()
     //** toma el id de la url
     
     return (
         <div>
             <h1>Detalle de la tarea - {id}</h1>
+            <h2>{task.name}</h2>
+            <h3>{task.description }</h3>
         </div>
     )
 }
